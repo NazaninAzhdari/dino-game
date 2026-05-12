8c90f3e846053616cace8f7c296bd8b742c222e5
Naz <Nazanin.Azhdari.sbu@gmail.com>
2026-05-12 18:05:09 +0300

    synchronizing the VGA

---
+++ HVsync.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library work;
+use work.dino_pack.ALL;
+
+entity HVsync is
+    port (
+        i_clk25     :       in      STD_LOGIC;  --25MHz
+        i_reset     :       in      STD_LOGIC;
+        o_x         :       out     unsigned(pc_VGA_BITS -1 downto 0);
+        o_y         :       out     unsigned(pc_VGA_BITS -1 downto 0);
+        o_HS        :       out     STD_LOGIC;
+        o_VS        :       out     STD_LOGIC;
+        o_DE        :       out     STD_LOGIC
+    );
+end HVsync;
+
+architecture RTL of HVsync is
+    signal r_x  :   integer range 0 to pc_H_TOTAL -1  :=0;
+    signal r_Y  :   integer range 0 to pc_V_TOTAL -1  :=0;
+
+    begin
+        process(i_clk25, i_reset) is
+            begin
+                if i_reset = '1' then
+                    r_x <= 0;
+                    r_y <= 0;
+                
+                elsif rising_edge(i_clk25) then
+                    if r_y < pc_V_TOTAL-1 then 
+                        if r_x < pc_H_TOTAL -1 then
+                            r_x <= r_x + 1;
+                        else
+                            r_x <= 0;
+                            r_y <= r_y + 1;
+                        end if;
+                    else
+                        r_y <= 0;
+                    end if;
+                end if;
+        end process;
+
+        o_HS <= '0' when (r_x >= pc_H_ACTIVE + pc_H_FP) and (r_x < pc_H_TOTAL - pc_H_BP) else '1';
+        o_VS <= '0' when (r_y >= pc_V_ACTIVE + pc_V_FP) and (r_y < pc_V_TOTAL - pc_V_BP) else '1';
+        o_DE <= '1' when (r_x <= pc_H_ACTIVE -1) and (r_y <= pc_V_ACTIVE -1) else '0';
+
+        o_x <= to_unsigned(r_x, o_x'length);
+        o_y <= to_unsigned(r_y, o_y'length);
+    end RTL;
